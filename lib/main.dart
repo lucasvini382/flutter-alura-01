b1edd6a982a64cd87d8d2522c2b2bb8522ab915b
Lucas Oliveira <lucasvini382@gmail.com>
2022-12-25 15:41:01 -0300

    feature: :sparkles: Adiciona InheritedWidget e Tela de formulario para cadastrar nova tarefa.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:alura_flutter_curso_1/data/task_inherited.dart';
 import 'package:alura_flutter_curso_1/screens/initial_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -15,10 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const InitialScreen(),
+      home: TaskInherited(child: const InitialScreen()),
     );
   }
 }
-
-
-
